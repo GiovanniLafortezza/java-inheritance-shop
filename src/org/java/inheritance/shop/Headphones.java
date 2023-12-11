443ec4d6f39b35551b0f48a7e4ac694fd1deee7c
GiovanniLafortezza <recordeknot388@gmail.com>
2023-12-11 15:08:11 +0100

    create class Headphones

--- src/org/java/inheritance/shop/Headphones.java
+++ src/org/java/inheritance/shop/Headphones.java
@@ -1,4 +1,34 @@
 package org.java.inheritance.shop;
 
-public class Headphones {
+import java.math.BigDecimal;
+
+public class Headphones extends Product{
+    private String color;
+    private String choice;
+
+    // COSTRUTTORI
+    public Headphones(String name, String description, BigDecimal price, BigDecimal vat, Category category, String color, String choice) throws IllegalArgumentException {
+        super(name, description, price, vat, category);
+        this.color = color;
+        this.choice = choice;
+    }
+
+    //GETTER E SETTER
+
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getChoice() {
+        return choice;
+    }
+
+    public void setChoice(String choice) {
+        this.choice = choice;
+    }
 }
