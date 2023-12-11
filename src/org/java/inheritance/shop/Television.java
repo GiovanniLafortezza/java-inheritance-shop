0acc73c383ec0dbccdccec521e91d8a5570afef1
GiovanniLafortezza <recordeknot388@gmail.com>
2023-12-11 15:01:54 +0100

    create class Television

--- src/org/java/inheritance/shop/Television.java
+++ src/org/java/inheritance/shop/Television.java
@@ -1,4 +1,38 @@
 package org.java.inheritance.shop;
 
-public class Television {
+import java.math.BigDecimal;
+
+public class Television extends Product{
+    // ATTRIBUTI
+    private int size;
+    private boolean smart;
+
+    // COSTRUTTORI
+
+    public Television(String name, String description, BigDecimal price, BigDecimal vat, Category category, int size, boolean smart) throws IllegalArgumentException {
+        super(name, description, price, vat, category);
+        this.size = size;
+        this.smart = smart;
+    }
+
+
+    //GETTER E SETTER
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public boolean isSmart() {
+        return smart;
+    }
+
+    public void setSmart(boolean smart) {
+        this.smart = smart;
+    }
+
+    //METODI
 }
