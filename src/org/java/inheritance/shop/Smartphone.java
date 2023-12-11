4b8305cf1f5fff482330df24f19d822375f40e6e
GiovanniLafortezza <recordeknot388@gmail.com>
2023-12-11 14:56:45 +0100

    create class Smartphone

--- src/org/java/inheritance/shop/Smartphone.java
+++ src/org/java/inheritance/shop/Smartphone.java
@@ -1,4 +1,38 @@
 package org.java.inheritance.shop;
 
-public class Smartphone {
+import java.math.BigDecimal;
+import java.util.Random;
+
+public class Smartphone extends Product {
+    //ATTRIBUTI
+    private int codeImei;
+    private String memory;
+
+    //COSTRUTTORI
+
+
+    public Smartphone(String name, String description, BigDecimal price, BigDecimal vat, Category category, String memory) throws IllegalArgumentException {
+        super(name, description, price, vat, category);
+        this.codeImei = generateCode();
+        this.memory = memory;
+    }
+
+    //GETTER e SETTER
+
+    public String getMemory() {
+        return memory;
+    }
+
+    public void setMemory(String memory) {
+        this.memory = memory;
+    }
+
+    //METODI
+
+    private int generateCode() {
+        Random generateCode = new Random();
+        return generateCode.nextInt(10000000);
+    }
+
+
 }
